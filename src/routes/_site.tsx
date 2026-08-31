906e87c890fb3d42abaac987fee5155f100882fa
Shohail Mahmud <nirjonpc@gmail.com>
2026-08-31 23:37:23 +0600

    Remove ScrollToTop from SiteLayout
    Removed ScrollToTop component from SiteLayout.

--- src/routes/_site.tsx
+++ src/routes/_site.tsx
@@ -1,6 +1,5 @@
 import { createFileRoute } from "@tanstack/react-router";
 import { CartProvider } from "@/context/CartContext";
-import { ScrollToTop } from "@/components/ScrollToTop";
 import { Layout } from "@/components/Layout";
 
 export const Route = createFileRoute("/_site")({
@@ -10,7 +9,6 @@ export const Route = createFileRoute("/_site")({
 function SiteLayout() {
   return (
     <CartProvider>
-      <ScrollToTop />
       <Layout />
     </CartProvider>
   );
